ad05a3edfe4c45717abc32bb017c44199cc6735e
Ted <tedv@live.com>
2024-03-25 16:00:32 +0100

    update

--- infra/vnet.tf
+++ infra/vnet.tf
@@ -1,10 +1,14 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet1"
-  resource_group_name = "rgplatform101125"
-  location            = "swedencentral"
+  resource_group_name = var.resource_group_name
+  location            = data.azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
-variable env_id {
+variable resource_group_name {
+}
+
+data azurerm_resource_group rg {
+  name = var.resource_group_name
 }
 
