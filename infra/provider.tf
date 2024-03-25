6dc3c24ce563e58e1294e007fdfd6a6e50d2a050
Ted <tedv@live.com>
2024-03-25 11:30:32 +0100

    update

--- infra/provider.tf
+++ infra/provider.tf
@@ -20,13 +20,6 @@ terraform {
 provider "azurerm" {
   features {
   }
-  subscription_id = var.subscription_id
-  use_oidc        = true
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
 }
 
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
 
